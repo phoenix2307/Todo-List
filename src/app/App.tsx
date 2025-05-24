5d4dcdd90438b4c3207e49e666c22f992845bb0e
Alex <phoenix.trade2307@gmail.com>
2025-05-24 17:12:58 +0300

    me-initialize update with CircularProgress

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,30 +1,43 @@
-import "./App.css"
-import { selectThemeMode } from "@/app/app-slice"
-import { ErrorSnackbar, Header } from "@/common/components"
+// import "./App.css"
+import {selectThemeMode} from "@/app/app-slice"
+import {ErrorSnackbar, Header} from "@/common/components"
 import {useAppDispatch, useAppSelector} from "@/common/hooks"
-import { Routing } from "@/common/routing"
-import { getTheme } from "@/common/theme"
+import {Routing} from "@/common/routing"
+import {getTheme} from "@/common/theme"
 import CssBaseline from "@mui/material/CssBaseline"
-import { ThemeProvider } from "@mui/material/styles"
-import {useEffect} from "react";
+import {ThemeProvider} from "@mui/material/styles"
+import {useEffect, useState} from "react";
 import {initializeAppTC} from "@/features/auth/model/auth-slice";
+import {CircularProgress} from "@mui/material";
+import styles from './App.module.css'
 
 export const App = () => {
-  const themeMode = useAppSelector(selectThemeMode)
-  const theme = getTheme(themeMode)
-  const dispatch = useAppDispatch()
-  useEffect(()=>{
-    dispatch(initializeAppTC())
-  }, [])
+    const [isInitialized, setIsInitialized] = useState(false)
+    const themeMode = useAppSelector(selectThemeMode)
+    const theme = getTheme(themeMode)
+    const dispatch = useAppDispatch()
+    useEffect(() => {
+        dispatch(initializeAppTC()).finally(()=>{
+            setIsInitialized(true)
+        })
+    }, [])
 
-  return (
-    <ThemeProvider theme={theme}>
-      <div className={"app"}>
-        <CssBaseline />
-        <Header />
-        <Routing />
-        <ErrorSnackbar />
-      </div>
-    </ThemeProvider>
-  )
+    if (!isInitialized) {
+        return (
+            <div className={styles.circularProgressContainer}>
+                <CircularProgress size={150} thickness={3}/>
+            </div>
+        )
+    }
+
+    return (
+        <ThemeProvider theme={theme}>
+            <div className={styles.app}>
+                <CssBaseline/>
+                <Header/>
+                <Routing/>
+                <ErrorSnackbar/>
+            </div>
+        </ThemeProvider>
+    )
 }
