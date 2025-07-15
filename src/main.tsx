b6caaf0c7d8017f46da7c5900e7688d9b09bb672
Alex <phoenix.trade2307@gmail.com>
2025-07-15 23:39:43 +0300

    hashrouter

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,13 @@ import { App } from "@/app/App"
 import { createRoot } from "react-dom/client"
 import "./index.css"
 import { Provider } from "react-redux"
-import { BrowserRouter } from "react-router"
+import { HashRouter } from "react-router"
 import { store } from "./app/store"
 
 createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
+  <HashRouter >
     <Provider store={store}>
       <App />
     </Provider>
-  </BrowserRouter>,
+  </HashRouter>,
 )
